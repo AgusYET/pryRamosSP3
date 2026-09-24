31110f5778d229619b842784f22304d939374f14
AgusYET <ramosagustin885@gmail.com>
2026-09-24 09:51:40 -0300

    Hice varios cambios con los radios, y ahora me falta algo mas con los vecRequisitos

--- pryRamosSP3/Form1.Designer.cs
+++ pryRamosSP3/Form1.Designer.cs
@@ -32,28 +32,30 @@
             lblTitulo = new Label();
             tbp = new TabControl();
             tbpCarga = new TabPage();
-            tbpBusqueda = new TabPage();
+            rbImportado = new RadioButton();
+            rbNacional = new RadioButton();
+            lstRepuestos = new ListBox();
             btnGuardar = new Button();
             lblPrecio = new Label();
             lblDescripcion = new Label();
             lblNumero = new Label();
-            lblOrigen = new Label();
             lblMarca = new Label();
-            cmbOrigen = new ComboBox();
             cmbMarca = new ComboBox();
             txtPrecios = new TextBox();
             txtDescripcion = new TextBox();
             txtNumeroRepuesto = new TextBox();
+            tbpBusqueda = new TabPage();
             gbOpciones = new GroupBox();
+            rdbImportado = new RadioButton();
+            rdbNacional = new RadioButton();
             cmbMarca2 = new ComboBox();
-            rbOrigen2 = new RadioButton();
-            rbOrigen = new RadioButton();
             lblBusqueda = new Label();
-            lstRepuestos = new ListBox();
+            gbOrigen = new GroupBox();
             tbp.SuspendLayout();
             tbpCarga.SuspendLayout();
             tbpBusqueda.SuspendLayout();
             gbOpciones.SuspendLayout();
+            gbOrigen.SuspendLayout();
             SuspendLayout();
             // 
             // lblTitulo
@@ -78,14 +80,14 @@
             // 
             // tbpCarga
             // 
+            tbpCarga.BackColor = SystemColors.ActiveCaption;
+            tbpCarga.Controls.Add(gbOrigen);
             tbpCarga.Controls.Add(lstRepuestos);
             tbpCarga.Controls.Add(btnGuardar);
             tbpCarga.Controls.Add(lblPrecio);
             tbpCarga.Controls.Add(lblDescripcion);
             tbpCarga.Controls.Add(lblNumero);
-            tbpCarga.Controls.Add(lblOrigen);
             tbpCarga.Controls.Add(lblMarca);
-            tbpCarga.Controls.Add(cmbOrigen);
             tbpCarga.Controls.Add(cmbMarca);
             tbpCarga.Controls.Add(txtPrecios);
             tbpCarga.Controls.Add(txtDescripcion);
@@ -96,19 +98,36 @@
             tbpCarga.Size = new Size(529, 391);
             tbpCarga.TabIndex = 0;
             tbpCarga.Text = "Carga De Repuestos";
-            tbpCarga.UseVisualStyleBackColor = true;
             // 
-            // tbpBusqueda
+            // rbImportado
             // 
-            tbpBusqueda.Controls.Add(gbOpciones);
-            tbpBusqueda.Controls.Add(lblBusqueda);
-            tbpBusqueda.Location = new Point(4, 24);
-            tbpBusqueda.Name = "tbpBusqueda";
-            tbpBusqueda.Padding = new Padding(3);
-            tbpBusqueda.Size = new Size(529, 391);
-            tbpBusqueda.TabIndex = 1;
-            tbpBusqueda.Text = "Busqueda de repuestos";
-            tbpBusqueda.UseVisualStyleBackColor = true;
+            rbImportado.AutoSize = true;
+            rbImportado.Location = new Point(210, 10);
+            rbImportado.Name = "rbImportado";
+            rbImportado.Size = new Size(92, 19);
+            rbImportado.TabIndex = 28;
+            rbImportado.TabStop = true;
+            rbImportado.Text = "IMPORTADO";
+            rbImportado.UseVisualStyleBackColor = true;
+            // 
+            // rbNacional
+            // 
+            rbNacional.AutoSize = true;
+            rbNacional.Location = new Point(119, 10);
+            rbNacional.Name = "rbNacional";
+            rbNacional.Size = new Size(85, 19);
+            rbNacional.TabIndex = 27;
+            rbNacional.TabStop = true;
+            rbNacional.Text = "NACIONAL";
+            rbNacional.UseVisualStyleBackColor = true;
+            // 
+            // lstRepuestos
+            // 
+            lstRepuestos.FormattingEnabled = true;
+            lstRepuestos.Location = new Point(6, 306);
+            lstRepuestos.Name = "lstRepuestos";
+            lstRepuestos.Size = new Size(502, 79);
+            lstRepuestos.TabIndex = 26;
             // 
             // btnGuardar
             // 
@@ -119,13 +138,14 @@
             btnGuardar.TabIndex = 20;
             btnGuardar.Text = "Guardar";
             btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
             // 
             // lblPrecio
             // 
             lblPrecio.AutoSize = true;
             lblPrecio.BackColor = Color.White;
             lblPrecio.Font = new Font("Yu Gothic", 12F, FontStyle.Bold | FontStyle.Italic);
-            lblPrecio.Location = new Point(81, 211);
+            lblPrecio.Location = new Point(122, 253);
             lblPrecio.Name = "lblPrecio";
             lblPrecio.Size = new Size(69, 21);
             lblPrecio.TabIndex = 25;
@@ -147,83 +167,98 @@
             lblNumero.AutoSize = true;
             lblNumero.BackColor = Color.White;
             lblNumero.Font = new Font("Yu Gothic", 12F, FontStyle.Bold | FontStyle.Italic);
-            lblNumero.Location = new Point(78, 112);
+            lblNumero.Location = new Point(109, 114);
             lblNumero.Name = "lblNumero";
             lblNumero.Size = new Size(82, 21);
             lblNumero.TabIndex = 23;
             lblNumero.Text = "Numero: ";
-            // 
-            // lblOrigen
-            // 
-            lblOrigen.AutoSize = true;
-            lblOrigen.BackColor = Color.White;
-            lblOrigen.Font = new Font("Yu Gothic", 12F, FontStyle.Bold | FontStyle.Italic);
-            lblOrigen.Location = new Point(78, 61);
-            lblOrigen.Name = "lblOrigen";
-            lblOrigen.Size = new Size(71, 21);
-            lblOrigen.TabIndex = 22;
-            lblOrigen.Text = "Origen: ";
             // 
             // lblMarca
             // 
             lblMarca.AutoSize = true;
             lblMarca.BackColor = Color.White;
             lblMarca.Font = new Font("Yu Gothic", 12F, FontStyle.Bold | FontStyle.Italic);
-            lblMarca.Location = new Point(78, 12);
+            lblMarca.Location = new Point(123, 14);
             lblMarca.Name = "lblMarca";
             lblMarca.Size = new Size(68, 21);
             lblMarca.TabIndex = 21;
             lblMarca.Text = "Marca: ";
             // 
-            // cmbOrigen
-            // 
-            cmbOrigen.FormattingEnabled = true;
-            cmbOrigen.Items.AddRange(new object[] { "I", "N" });
-            cmbOrigen.Location = new Point(221, 59);
-            cmbOrigen.Name = "cmbOrigen";
-            cmbOrigen.Size = new Size(121, 23);
-            cmbOrigen.TabIndex = 16;
-            // 
             // cmbMarca
             // 
             cmbMarca.FormattingEnabled = true;
             cmbMarca.Items.AddRange(new object[] { "F", "P", "R" });
-            cmbMarca.Location = new Point(221, 12);
+            cmbMarca.Location = new Point(202, 15);
             cmbMarca.Name = "cmbMarca";
             cmbMarca.Size = new Size(121, 23);
             cmbMarca.TabIndex = 15;
             // 
             // txtPrecios
             // 
-            txtPrecios.Location = new Point(202, 212);
+            txtPrecios.Location = new Point(202, 254);
             txtPrecios.Name = "txtPrecios";
-            txtPrecios.Size = new Size(174, 23);
+            txtPrecios.Size = new Size(103, 23);
             txtPrecios.TabIndex = 19;
             // 
             // txtDescripcion
             // 
             txtDescripcion.Location = new Point(202, 161);
+            txtDescripcion.Multiline = true;
             txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(174, 23);
+            txtDescripcion.Size = new Size(223, 74);
             txtDescripcion.TabIndex = 18;
             // 
             // txtNumeroRepuesto
             // 
             txtNumeroRepuesto.Location = new Point(202, 112);
             txtNumeroRepuesto.Name = "txtNumeroRepuesto";
-            txtNumeroRepuesto.Size = new Size(174, 23);
+            txtNumeroRepuesto.Size = new Size(121, 23);
             txtNumeroRepuesto.TabIndex = 17;
+            // 
+            // tbpBusqueda
+            // 
+            tbpBusqueda.BackColor = SystemColors.ActiveCaption;
+            tbpBusqueda.Controls.Add(gbOpciones);
+            tbpBusqueda.Controls.Add(lblBusqueda);
+            tbpBusqueda.Location = new Point(4, 24);
+            tbpBusqueda.Name = "tbpBusqueda";
+            tbpBusqueda.Padding = new Padding(3);
+            tbpBusqueda.Size = new Size(529, 391);
+            tbpBusqueda.TabIndex = 1;
+            tbpBusqueda.Text = "Busqueda de repuestos";
             // 
             // gbOpciones
             // 
+            gbOpciones.Controls.Add(rdbImportado);
+            gbOpciones.Controls.Add(rdbNacional);
             gbOpciones.Controls.Add(cmbMarca2);
-            gbOpciones.Controls.Add(rbOrigen2);
-            gbOpciones.Controls.Add(rbOrigen);
             gbOpciones.Location = new Point(27, 154);
             gbOpciones.Name = "gbOpciones";
             gbOpciones.Size = new Size(306, 120);
             gbOpciones.TabIndex = 18;
             gbOpciones.TabStop = false;
+            // 
+            // rdbImportado
+            // 
+            rdbImportado.AutoSize = true;
+            rdbImportado.Location = new Point(181, 72);
+            rdbImportado.Name = "rdbImportado";
+            rdbImportado.Size = new Size(92, 19);
+            rdbImportado.TabIndex = 30;
+            rdbImportado.TabStop = true;
+            rdbImportado.Text = "IMPORTADO";
+            rdbImportado.UseVisualStyleBackColor = true;
+            // 
+            // rdbNacional
+            // 
+            rdbNacional.AutoSize = true;
+            rdbNacional.Location = new Point(90, 72);
+            rdbNacional.Name = "rdbNacional";
+            rdbNacional.Size = new Size(85, 19);
+            rdbNacional.TabIndex = 29;
+            rdbNacional.TabStop = true;
+            rdbNacional.Text = "NACIONAL";
+            rdbNacional.UseVisualStyleBackColor = true;
             // 
             // cmbMarca2
             // 
@@ -233,28 +268,6 @@
             cmbMarca2.Name = "cmbMarca2";
             cmbMarca2.Size = new Size(121, 23);
             cmbMarca2.TabIndex = 2;
-            // 
-            // rbOrigen2
-            // 
-            rbOrigen2.AutoSize = true;
-            rbOrigen2.Location = new Point(195, 66);
-            rbOrigen2.Name = "rbOrigen2";
-            rbOrigen2.Size = new Size(92, 19);
-            rbOrigen2.TabIndex = 1;
-            rbOrigen2.TabStop = true;
-            rbOrigen2.Text = "IMPORTADO";
-            rbOrigen2.UseVisualStyleBackColor = true;
-            // 
-            // rbOrigen
-            // 
-            rbOrigen.AutoSize = true;
-            rbOrigen.Location = new Point(195, 29);
-            rbOrigen.Name = "rbOrigen";
-            rbOrigen.Size = new Size(85, 19);
-            rbOrigen.TabIndex = 0;
-            rbOrigen.TabStop = true;
-            rbOrigen.Text = "NACIONAL";
-            rbOrigen.UseVisualStyleBackColor = true;
             // 
             // lblBusqueda
             // 
@@ -266,13 +279,16 @@
             lblBusqueda.TabIndex = 20;
             lblBusqueda.Text = "BUSQUEDA DE REPUESTOS";
             // 
-            // lstRepuestos
+            // gbOrigen
             // 
-            lstRepuestos.FormattingEnabled = true;
-            lstRepuestos.Location = new Point(35, 308);
-            lstRepuestos.Name = "lstRepuestos";
-            lstRepuestos.Size = new Size(441, 64);
-            lstRepuestos.TabIndex = 26;
+            gbOrigen.Controls.Add(rbImportado);
+            gbOrigen.Controls.Add(rbNacional);
+            gbOrigen.Location = new Point(83, 44);
+            gbOrigen.Name = "gbOrigen";
+            gbOrigen.Size = new Size(353, 58);
+            gbOrigen.TabIndex = 29;
+            gbOrigen.TabStop = false;
+            gbOrigen.Text = "Origen";
             // 
             // Repuestos
             // 
@@ -284,7 +300,7 @@
             Controls.Add(lblTitulo);
             ForeColor = SystemColors.ActiveCaptionText;
             Name = "Repuestos";
-            Text = "RepuestosSP3";
+            Text = "Ventas De Reouestos";
             tbp.ResumeLayout(false);
             tbpCarga.ResumeLayout(false);
             tbpCarga.PerformLayout();
@@ -292,6 +308,8 @@
             tbpBusqueda.PerformLayout();
             gbOpciones.ResumeLayout(false);
             gbOpciones.PerformLayout();
+            gbOrigen.ResumeLayout(false);
+            gbOrigen.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -307,17 +325,18 @@
         private Label lblPrecio;
         private Label lblDescripcion;
         private Label lblNumero;
-        private Label lblOrigen;
         private Label lblMarca;
-        private ComboBox cmbOrigen;
         private ComboBox cmbMarca;
         private TextBox txtPrecios;
         private TextBox txtDescripcion;
         private TextBox txtNumeroRepuesto;
         private GroupBox gbOpciones;
         private ComboBox cmbMarca2;
-        private RadioButton rbOrigen2;
-        private RadioButton rbOrigen;
         private Label lblBusqueda;
+        private RadioButton rbImportado;
+        private RadioButton rbNacional;
+        private RadioButton rdbImportado;
+        private RadioButton rdbNacional;
+        private GroupBox gbOrigen;
     }
 }
--- pryRamosSP3/Form1.cs
+++ pryRamosSP3/Form1.cs
@@ -2,6 +2,20 @@ namespace pryRamosSP3
 {
     public partial class Repuestos : Form
     {
+        //declaracion de valores globales
+        //array de 1 dimension - vector - sin elementos
+        string[] vecRegistros;
+
+        //array de 2 dimensiones - matriz - sin elementos
+        string[] matRegistros;
+
+        //array de 1 dimension - vector
+        string[] vecRepuestos = new string[3];
+
+        //array de 2 dimensiones - matriz
+        string[,] matRepuestos = new string[2, 2];
+
+        
         public Repuestos()
         {
             InitializeComponent();
@@ -11,5 +25,22 @@ namespace pryRamosSP3
         string Descripcion;
         int Numero;
         int Precio;
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string varMarca = cmbMarca.Text;
+            string varOrigen;
+
+            varOrigen = (rbNacional.Checked == true) ? "Nacional" : "Importado";
+
+       
+
+            lstRepuestos.Items.Add(varMarca + ' ' + varOrigen);
+
+            //grabar en el vector . array de 1 dimension
+            vecRepuestos[0] = "primerdato";
+            vecRepuestos[1] = "segundodato";
+            vecRepuestos[2] = "tercerdato";
+        }
     }
 }
